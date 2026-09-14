c1c07c2e45a5b64086299eca80a168f177cd42ec
solitary <solitaryurt@gmail.com>
2026-09-14 14:13:29 -0700

    Add explicit-delta CharacterBody3D movement

--- scene/3d/physics/character_body_3d.cpp
+++ scene/3d/physics/character_body_3d.cpp
@@ -43,6 +43,11 @@
 bool CharacterBody3D::move_and_slide() {
 	// Hack in order to work with calling from _process as well as from _physics_process; calling from thread is risky
 	double delta = Engine::get_singleton()->is_in_physics_frame() ? get_physics_process_delta_time() : get_process_delta_time();
+	return move_and_slide_with_delta(delta);
+}
+
+bool CharacterBody3D::move_and_slide_with_delta(double delta) {
+	ERR_FAIL_COND_V_MSG(!Math::is_finite(delta) || delta <= 0.0, false, "Character movement delta must be finite and positive.");
 
 	for (int i = 0; i < 3; i++) {
 		if (locked_axis & (1 << i)) {
@@ -868,6 +873,7 @@ void CharacterBody3D::_notification(int p_what) {
 
 void CharacterBody3D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("move_and_slide"), &CharacterBody3D::move_and_slide);
+	ClassDB::bind_method(D_METHOD("move_and_slide_with_delta", "delta"), &CharacterBody3D::move_and_slide_with_delta);
 	ClassDB::bind_method(D_METHOD("apply_floor_snap"), &CharacterBody3D::apply_floor_snap);
 
 	ClassDB::bind_method(D_METHOD("set_velocity", "velocity"), &CharacterBody3D::set_velocity);
